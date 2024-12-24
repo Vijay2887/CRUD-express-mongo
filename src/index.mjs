280d87be9f56c6297b8c9ce4ad3f5931deaebcf0
Vijay2887 <pulipativijayasimha@gmail.com>
2024-12-24 12:49:50 +0530

    modified index.mjs and userRoute.mjs

--- src/index.mjs
+++ src/index.mjs
@@ -1,17 +1,20 @@
 import express from "express";
-import mongoose from "mongoose";
+import connectDb from "./connection.mjs";
 import userRoute from "./routes/userRoute.mjs";
+
 const app = express();
-
 app.use(express.json());
-app.use(userRoute);
 
-mongoose
-  .connect("mongodb://localhost:27017/CRUD_Operations")
+//database connection
+connectDb
   .then(() => console.log("Database Connected"))
   .catch((error) => console.log(error));
 
+//listen to the port
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Running at port ${PORT}`);
 });
+
+//routes
+app.use(userRoute);
--- src/routes/userRoute.mjs
+++ src/routes/userRoute.mjs
@@ -1,6 +1,11 @@
-import { request, response, Router } from "express";
-import { User } from "../database/Schemas/userSchema.mjs";
-import { hashPassword } from "../utils/hashPasswords.mjs";
+import { Router } from "express";
+import {
+  addUserToDb,
+  deleteUser,
+  getAllUsers,
+  singleUser,
+  updateUser,
+} from "../Controller/User/userControllers.mjs";
 
 const router = Router();
 
@@ -9,87 +14,18 @@ router.get("/", (request, response) => {
 });
 
 // Add a user to the database
-router.post("/api/users", (request, response) => {
-  const { body } = request;
-  body.password = hashPassword(body.password);
-  const newUser = User(body)
-    .save()
-    .then(() => console.log("User successfully added to database"))
-    .catch((error) => {
-      console.log(error);
-      return response.status(400);
-    });
-  return response.status(200).json(body);
-});
+router.post("/api/users", addUserToDb);
 
 //get all users
-router.get("/api/users", async (request, response) => {
-  try {
-    const users = await User.find({}, { password: false });
-    if (!users) return response.status(200).send({ msg: "No data available" });
-    return response.status(200).json(users);
-  } catch (error) {
-    response.sendStatus(400);
-  }
-});
+router.get("/api/users", getAllUsers);
 
 //get a single user through email
-router.get("/api/users/:email", async (request, response) => {
-  try {
-    const { email } = request.params;
-
-    const findUser = await User.find({ email: email }, { password: false });
-    if (!findUser)
-      return response.status(400).send({ msg: "No such user found" });
-    response.status(200).send(findUser);
-  } catch (error) {
-    response.sendStatus(400);
-  }
-});
+router.get("/api/users/:email", singleUser);
 
 // update a user using email
-router.put("/api/users/:email", async (request, response) => {
-  try {
-    const { body } = request;
-    const { email } = request.params;
-    const updatedUser = await User.updateOne({ email: email }, { $set: body });
-    if (!updatedUser.matchedCount === 1)
-      return response.status(400).send({ msg: "No such user found" });
-
-    response.status(200).send(updatedUser);
-  } catch (error) {
-    response.sendStatus(400);
-  }
-});
-
-// this router wont work as the above router creates ambiguity.. But this is the way to update through Id
-
-// router.put("/api/users/:id", async (request, response) => {
-//   try {
-//     const { body } = request;
-//     const { id } = request.params;
-
-//     const updatedUser = await User.updateOne({ _id: id }, { $set: body });
-//     if (!updatedUser.matchedCount === 1)
-//       return response.status(400).send({ msg: "No such user found" });
-
-//     response.status(200).send(updatedUser);
-//   } catch (error) {
-//     response.sendStatus(400);
-//   }
-// });
+router.put("/api/users/:email", updateUser);
 
 // delete a user through email
-router.delete("/api/users/:email", async (request, response) => {
-  try {
-    const { email } = request.params;
-    const deletedUser = await User.deleteOne({ email: email });
-    if (deletedUser.deletedCount !== 1)
-      return response.status(400).send({ msg: "No such user found" });
-    return response.send(deletedUser);
-  } catch (error) {
-    response.sendStatus(500);
-  }
-});
+router.delete("/api/users/:email", deleteUser);
 
 export default router;
